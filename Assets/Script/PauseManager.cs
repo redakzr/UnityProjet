9ae7fd977b04756e9ef30fa550f09dfe0e634f4f
redakzr <reda.khezzar@etu.Cyu.fr>
2026-02-20 14:48:57 +0100

    ajout de l'interface pause menu

--- Assets/Script/PauseManager.cs
+++ Assets/Script/PauseManager.cs
@@ -5,12 +5,23 @@ public class PauseManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
    
 private bool isGamePaused = false;
+  [SerializeField]
+    private GameObject pauseMenuUI;
+ 
+ 
+    private void Awake()
+    {
+        
+        pauseMenuUI.SetActive(false);
+
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             isGamePaused = !isGamePaused;
+            
             if (isGamePaused)
             {
                 Pause();
@@ -22,11 +33,14 @@ private bool isGamePaused = false;
         }}
     private void Pause()
     {
+        pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
+        
     }
  
-    private void Resume()
+    public void Resume()
     {
+        pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
     }
 }
