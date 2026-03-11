f623084521b5a7635ba34dfa90a6951db080ee58
redakzr <reda.khezzar@etu.cyu.fr>
2026-03-11 09:43:51 +0100

    Montrer quand le personnage perd de la vie

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -10,6 +10,9 @@ public class PlayerHealth : MonoBehaviour
     private int maxLifePoints;
     [SerializeField]
     private TextMeshProUGUI currentLifePointsText;
+
+    [SerializeField]
+
  
     private bool isInvulnerable = false;
  
@@ -32,15 +35,49 @@ public class PlayerHealth : MonoBehaviour
         currentLifePoints - 1,
         0,
         maxLifePoints);
+        StartCoroutine(InvulnerableFlash());
         currentLifePointsText.SetText(currentLifePoints.ToString());
     }
  
-    IEnumerator InvulnerableFlash()
+   IEnumerator InvulnerableFlash()
     {
         isInvulnerable = true;
  
-        yield return new WaitForSeconds(3);
-        Debug.Log("Fini");
-    }
+        // Durée de l'invulnerabilité
+        float invulnerableDuration = 1.25f;
+        // Temps écoulé durant la période d'invulnerabilité
+        float timeElapsed = 0;
  
+        // Durée durant laquelle le sprite est visible ou invisible
+        float flashInvulnerabilityDuration = 0.2f;
+        // Temps écoulé durant la période de visibilité ou invisibilité
+        float flashTimeElapsed = 0;
+        bool isVisible = true;
+ 
+        while (timeElapsed < invulnerableDuration)
+        {
+            timeElapsed += Time.deltaTime;
+            flashTimeElapsed += Time.deltaTime;
+ 
+            if (flashTimeElapsed >= flashInvulnerabilityDuration)
+            {
+                if (isVisible)
+                {
+                    sr.color = Color.clear;
+                } else
+                {
+                    sr.color = Color.white;
+                }
+ 
+                flashTimeElapsed = 0;
+                isVisible = !isVisible;
+            }
+ 
+            // Attendre le rafraichissement de l'écran
+            yield return null;
+        }
+ 
+        sr.color = Color.white;
+        isInvulnerable = false;
+    }
 }
