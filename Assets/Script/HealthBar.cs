cbd600e5cd9e9b9ee50f70b2e5e1e9806a8df61e
redakzr <reda.khezzar@etu.cyu.fr>
2026-03-11 10:33:07 +0100

    re actualiser

--- Assets/Script/HealthBar.cs
+++ Assets/Script/HealthBar.cs
@@ -5,11 +5,13 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField]
     private Image fillImage;
+    [SerializeField]
+    private Gradient gradient;
  
     
     public void SetHealth(float healthNormalized)
     {
         fillImage.fillAmount = healthNormalized;
-        
+        fillImage.color = gradient.Evaluate(healthNormalized);
     }
 }
